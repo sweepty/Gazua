7861cbdc083be0cf9062b264ef5c32a8de5063a5
Seungyeon Lee <adie0423@gmail.com>
2018-12-04 17:04:32 +0900

    [수정] apikey 숨기기

--- src/API/base-URL.js
+++ src/API/base-URL.js
@@ -1,3 +1,5 @@
+import apikey from './api-key';
+
 export const ROOT_URL = 'https://min-api.cryptocompare.com/data/';
 export const SECOND_URL = 'https://chasing-coins.com/api/v1/';
-export const EXCHANGE_RATE_URL = 'http://www.apilayer.net/api/live?access_key=91acf046964528aa2f3096fb473cb208';
+export const EXCHANGE_RATE_URL = `http://www.apilayer.net/api/live?access_key=${apikey}`;
